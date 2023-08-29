873f47367787911fcc4f42d48fdacdb393e0a1c9
Wei Jin <58967750+weijin1990@users.noreply.github.com>
2023-08-29 17:47:02 +0900

    Add files via upload

--- src/components/pages/HelloWorld.vue
+++ src/components/pages/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0)
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="@/assets/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
