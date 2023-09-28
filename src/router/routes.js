38370f330d92dfddbe540cde2084b96dc6326a5e
Wei Jin <58967750+weijin1990@users.noreply.github.com>
2023-09-28 17:25:30 +0900

    Add files via upload

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,11 @@
 import HelloWorld from '@/components/pages/HelloWorld.vue'
 import Login from '@/components/pages/Login.vue';
 import FetchAPIData from '@/components/pages/FetchAPIData.vue';
+import Dashboard from '@/components/pages/Dashboard.vue';
 
 export default [
   { path: '/', component: Login, meta: { title: 'ログイン|Fics' }},
   { path: '/home', component: HelloWorld, meta: { title: 'ホーム|Fics' }},
   { path: '/fetch', component: FetchAPIData, meta: { title: 'APIテスト|Fics' } },
+  { path: '/dashboard', component: Dashboard, meta: { title: 'Dashboard|Fics' } },
 ];
