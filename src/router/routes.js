f05f7833e9a56d16605eb386c035a89dde22b157
WEI JIN <weijin_jp@qq.com>
2023-10-08 14:03:44 +0900

    init front APP for POC

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,8 +4,9 @@ import FetchAPIData from '@/components/pages/FetchAPIData.vue';
 import Dashboard from '@/components/pages/Dashboard.vue';
 
 export default [
-  { path: '/', component: Login, meta: { title: 'ログイン|Fics' }},
-  { path: '/home', component: HelloWorld, meta: { title: 'ホーム|Fics' }},
+  // { path: '/', component: Login, meta: { title: 'ログイン|Fics' }},
+  // { path: '/home', component: HelloWorld, meta: { title: 'ホーム|Fics' }},
   { path: '/fetch', component: FetchAPIData, meta: { title: 'APIテスト|Fics' } },
-  { path: '/dashboard', component: Dashboard, meta: { title: 'Dashboard|Fics' } },
+  // { path: '/dashboard', component: Dashboard, meta: { title: 'Dashboard|Fics' } },
+  { path: '/', component: Dashboard, meta: { title: 'Homeページ|Fics' } },
 ];
